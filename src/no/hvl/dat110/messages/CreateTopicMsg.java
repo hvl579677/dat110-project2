4bd6242fa6388b0dd66ed12acb60531da5beb0bf
Niklas V. Raa <niklas.vatshelle.raa@gmail.com>
2020-02-24 11:05:35 +0100

    start

--- src/no/hvl/dat110/messages/CreateTopicMsg.java
+++ src/no/hvl/dat110/messages/CreateTopicMsg.java
@@ -1,7 +1,27 @@
 package no.hvl.dat110.messages;
 
 public class CreateTopicMsg extends Message {
-	
+
+    private String topic;
+
+    public CreateTopicMsg(String user, String topic){
+        super(MessageType.CREATETOPIC, user);
+        this.topic = topic;
+    }
+
+    public String getTopic(){
+        return topic;
+    }
+
+    // setTopic;
+    public void setTopic(String topic){
+        this.topic = topic;
+    }
+
+    public String toString(){
+        return ("CreateTopicMsg [topic = " + topic + "]");
+    }
+
 	// message sent from client to create topic on the broker
 	
 	// TODO: 
