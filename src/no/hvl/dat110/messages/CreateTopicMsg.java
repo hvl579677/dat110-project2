fbb6fdf3361645734b696f1d81e8153d3442177b
Niklas V. Raa <niklas.vatshelle.raa@gmail.com>
2020-02-24 13:25:02 +0100

    Task A in progress

--- src/no/hvl/dat110/messages/CreateTopicMsg.java
+++ src/no/hvl/dat110/messages/CreateTopicMsg.java
@@ -2,6 +2,14 @@ package no.hvl.dat110.messages;
 
 public class CreateTopicMsg extends Message {
 
+    // message sent from client to create topic on the broker
+
+    // TODO:
+    // Implement object variables - a topic is required
+
+    // Constructor, get/set-methods, and toString method
+    // as described in the project text
+
     private String topic;
 
     public CreateTopicMsg(String user, String topic){
@@ -13,20 +21,11 @@ public class CreateTopicMsg extends Message {
         return topic;
     }
 
-    // setTopic;
     public void setTopic(String topic){
         this.topic = topic;
     }
 
-    public String toString(){
+    public String toString() {
         return ("CreateTopicMsg [topic = " + topic + "]");
     }
-
-	// message sent from client to create topic on the broker
-	
-	// TODO: 
-	// Implement object variables - a topic is required
-	
-	// Constructor, get/set-methods, and toString method
-    // as described in the project text	
 }
