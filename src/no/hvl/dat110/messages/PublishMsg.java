cffec8b7b9a98781a08aa189e77b3214f44e5ef9
Niklas V. Raa <niklas.vatshelle.raa@gmail.com>
2020-02-24 13:51:38 +0100

    task A finished

--- src/no/hvl/dat110/messages/PublishMsg.java
+++ src/no/hvl/dat110/messages/PublishMsg.java
@@ -9,5 +9,43 @@ public class PublishMsg extends Message {
 
 	// Constructor, get/set-methods, and toString method
 	// as described in the project text
-	
+
+    private String topic;
+    private String message;
+    private String user;
+
+    public PublishMsg(String user, String topic, String message){
+        this.topic = message;
+        this.message = message;
+        this.user = user;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return ("PublishMsg [user = " + user + ", topic = " + topic + ", message = " + message + "]");
+    }
 }
--- src/no/hvl/dat110/messages/UnsubscribeMsg.java
+++ src/no/hvl/dat110/messages/UnsubscribeMsg.java
@@ -9,5 +9,24 @@ public class UnsubscribeMsg extends Message {
 
 	// Constructor, get/set-methods, and toString method
 	// as described in the project text
-	
+
+    private String topic;
+
+    public UnsubscribeMsg(String user, String topic){
+        super(MessageType.UNSUBSCRIBE, user);
+        this.topic = topic;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    @Override
+    public String toString() {
+        return ("UnsubscribeMsg [topic = " + topic + "]");
+    }
 }
